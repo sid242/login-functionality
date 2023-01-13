722a2a36a3bc0d38779c57a5474c9a3cbb7f41e0
Siddharth Prajapati <siddharth.prajapati@tatvic.com>
2023-01-13 12:04:08 +0530

    nodemailer

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(cors())
 app.use(express.json())
 
 
-app.use("/api/auth", require("./routes/auth"))
-
-
 app.get("/", (req, res) => {
     res.send("Hello")
 })
+app.use("/api/auth", require("./routes/auth"))
+
+
 
 app.listen(port, () => {
     console.log(`listening at port no ${port}`);
